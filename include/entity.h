cc444588b7ee28fc7306e19849363f199f603d97
ViruzUniT <viruz.unit@gmail.com>
2024-02-17 22:48:29 +0100

    Added Vector2 Class

--- include/entity.h
+++ include/entity.h
@@ -2,6 +2,15 @@
 
 #include "SDL.h"
 
+struct Vector2 {
+ public:
+  Vector2() : x(0), y(0) {}
+  Vector2(double x, double y) : x(x), y(y) {}
+
+ private:
+  double x, y;
+};
+
 class Entity {
  public:
   // Entity();
