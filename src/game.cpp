2b60fb985d0633336b692d08bf31274533d16d53
ViruzUniT <viruz.unit@gmail.com>
2024-02-18 21:26:54 +0100

    Added WASD input

--- src/game.cpp
+++ src/game.cpp
@@ -7,17 +7,25 @@ void Game::HandleSDLEvents(Time& Time) {
         isRunning = false;
         break;
       case SDL_KEYDOWN:
-        std::cout << "Key Pressed" << std::endl;
+        std::cout << "Key Pressed: " << currentevent.key.keysym.sym << " Right: " << SDLK_RIGHT << "\n";
 
         switch (currentevent.key.keysym.sym) {
-          case SDLK_RIGHT:
-            std::cout << "Right was pressed" << std::endl;
+          case SDLK_w:
+            std::cout << "W was pressed\n";
+            break;
+          case SDLK_a:
+            std::cout << "A was pressed\n";
+            break;
+          case SDLK_s:
+            std::cout << "S was pressed\n";
+            break;
+          case SDLK_d:
+            std::cout << "D was pressed\n";
             break;
           default:
             break;
         }
         break;
-
       default:
         break;
     }
