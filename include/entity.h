f9b4f884fe68cfbcc08258285092a2c46972e434
ViruzUniT <viruz.unit@gmail.com>
2024-02-25 22:47:56 +0100

    Added Math header and moved Vector2 struct to it

--- include/entity.h
+++ include/entity.h
@@ -4,14 +4,7 @@
 
 #include "SDL.h"
 #include "eventhandler.h"
-
-struct Vector2 {
- public:
-  Vector2() : x(0), y(0) {}
-  Vector2(double x, double y) : x(x), y(y) {}
-
-  double x, y;
-};
+#include "math.h"
 
 class Entity {
  public:
