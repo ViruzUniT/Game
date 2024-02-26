3a20cce717fc2e1bcf698c9bc8c53d0917894fcf
ViruzUniT <viruz.unit@gmail.com>
2024-02-26 03:50:42 +0100

    Added Character movement (#2)
    * Enabled FPS cap and disabled printing in the console
    
    * Style Optimization:
      Removed the commented old Game and Time code
    
    * Added:
    Basic Key Recognition
    
    * Optimized:
    Moved the SDL Event Handler to its own method in the Game struct
    
    * IDK nothing i think
    
    * Added Vector2 Class
    
    * Included Vector2 in the Entity Class
    
    * Fixed the Errors with the Changes about the Vector2 Class
    
    * Added:
    Entity Copy Constructor
    Entity Move Constructor
    Entity Destructor
    
    Changed:
    Set code Optimization to fast
    
    * Repaired the Entity Deconsturctor
    
    * Optimized Renderer Class
    
    * Deleted comments
    
    * Removed Vector2 move and copy constructor
    
    * Removed Vector2 Copy and Move Constructor
    
    * Added WASD input
    
    * Added key release switch statement
    
    * Added an basic event handler
    
    * Added Math header and moved Vector2 struct to it
    
    * Added Vector3 and 4 to math header
    
    * Added Game destructor
    
    * modified entity constructor
    
    * Added Player class and included it everywhere
    
    * First Prototype of the Move Function
    
    * Try 2
    
    * Fixed Direction (inverted up and down)
    
    * DeltaTime fix and halfed Movement Speed
    
    * Multiplied delta time with 100(no snail mode)
    
    * Added Timer
    
    * ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <ostream>
 
@@ -5,15 +6,27 @@
 #include "../include/game.h"
 #include "C:/SDL2-w64/include/SDL2/SDL.h"
 
-void InitSDL() {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    std::cout << "SDL not working: " << SDL_GetError() << std::endl;
-    return;
+struct Timer {
+ public:
+  Timer() { m_StartTimePoint = std::chrono::high_resolution_clock::now(); }
+  ~Timer() { Stop(); }
+  void Stop() {
+    auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+    auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimePoint).time_since_epoch().count();
+    auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+    auto duration = end - start;
+    double ms = duration * 0.001;
+    double s = duration / 1000000.0;
+    std::cout << s << " s (" << ms << " ms, " << duration << " us)\n";
   }
-  std::cout << "SDL working" << std::endl;
-}
+
+ private:
+  std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimePoint;
+  };
 
 int main(int argc, char* argv[]) {
+  Timer* timer = new Timer();
   Game Game("Game v0.1", 825, 480);
   Time Time(Game.GetWindow());
 
@@ -21,7 +34,7 @@ int main(int argc, char* argv[]) {
 
   SDL_Texture* playerTexture = window.LoadTexture("F:/c++ game/sprites/Fighter/Idle.png");
   Player Player(Vector2(100, 100), playerTexture, Vector4(46, 47, 30, 81));
-
+  delete timer;
   while (Game.isRunning == true) {
     Time.StartMeasure();
 
