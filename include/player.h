3bd38aceb7b8410ebde3c4d8cbee6b39b291796c
ViruzUniT <viruz.unit@gmail.com>
2024-03-05 03:53:45 +0100

    Added utils.h but will delete later

--- include/player.h
+++ include/player.h
@@ -1,9 +1,9 @@
 #include "entity.h"
 
 class Player : public Entity {
-public:
-  Player(Vector2 position, GameTexture texture): Entity(position, texture){}
-  float MovementSpeed = 5.0f; 
-  
-private:
+ public:
+  Player(Vector2 position, GameTexture texture) : Entity(position, texture) {}
+  float MovementSpeed = 5.0f;
+
+ private:
 };
