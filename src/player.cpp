008218fdb8a3d648631bd21f690d145b614cd5f2
ViruzUniT <viruz.unit@gmail.com>
2024-03-27 04:15:01 +0100

    Added square func and fixed bug in movemendhandler

--- src/player.cpp
+++ src/player.cpp
@@ -1,6 +1,9 @@
 #include "../include/player.h"
 
+#include <cmath>
 #include <cstring>
+
+#include "../include/math.h"
 
 #define IDLE_ANIM_NAME "Idle"
 #define WALK_ANIM_NAME "Walk"
@@ -20,13 +23,28 @@ void Player::Move(Time& Time, Player& Player) {
 
   Vector2 Position = Player.GetPosition();
 
-  if (dir.up)
-    Position.y -= Player.MovementSpeed * Time.deltaTime * 100;
-  if (dir.down)
-    Position.y += Player.MovementSpeed * Time.deltaTime * 100;
-  if (dir.left)
-    Position.x -= Player.MovementSpeed * Time.deltaTime * 100;
-  if (dir.right)
-    Position.x += Player.MovementSpeed * Time.deltaTime * 100;
+  // I will improve it eventually
+  if (dir.up && dir.right) {
+    Position.y -= sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+    Position.x += sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.up && dir.left) {
+    Position.y -= sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+    Position.x -= sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.down && dir.left) {
+    Position.y += sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+    Position.x -= sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.down && dir.right) {
+    Position.y += sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+    Position.x += sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.up) {
+    Position.y -= sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.down) {
+    Position.y += sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.right) {
+    Position.x += sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  } else if (dir.left) {
+    Position.x -= sqrt(square(Player.MovementSpeed)) * Time.deltaTime * 100;
+  }
+
   Player.SetPosition(Position);
 }
