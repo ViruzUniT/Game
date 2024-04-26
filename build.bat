2fbf26823146cdc29930b3b042e60c0bbbc7c035
ViruzUniT <viruz.unit@gmail.com>
2024-04-26 13:00:27 +0200

    test

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+@echo "hello"
+
 g++ -c -std=c++17 -Ofast src/*.cpp -Wall -m64 -I C:/SDL2-w64/include && g++ *.o -o build\main.exe -L C:/SDL2-w64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image && .\build\main.exe
