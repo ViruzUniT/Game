459d782f8d2fcab612b8186ae1a82b6f259ef375
ViruzUniT <viruz.unit@gmail.com>
2024-05-11 01:46:49 +0200

    Modified folder structure to be easyier to build

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 @echo "hello"
-
-g++ -c -std=c++17 -Ofast src/*.cpp -Wall -m64 -I C:/SDL2-w64/include && g++ *.o -o build\main.exe -L C:/SDL2-w64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image && .\build\main.exe
+g++ -c -std=c++17 -Ofast src/*.cpp -Wall -m64 -I ./external-libs/SDL2-w64/include && g++ *.o -o build\main.exe -L ./external-libs/SDL2-w64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image && .\build\main.exe
--- debug.bat
+++ debug.bat
@@ -1,5 +1,14 @@
-g++ -c -std=c++17 -g -Ofast src/*.cpp -Wall -m64 -lstdc++ -I C:/SDL2-w64/include && g++ *.o -o build\debug.exe -L C:/SDL2-w64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image 
+g++ -c -std=c++17 -g -Ofast src/*.cpp -Wall -m64 -lstdc++ -I ./external-libs/SDL2-w64/include 
+
 @echo off
+copy "./SDL2.dll" "./build"
+copy "./SDL2_image.dll" "./build"
+robocopy "./sprites" "./build" /E 
+
+move ./*.o ./build-int
+
+g++ ./build-int/*.o -o build\debug.exe -L ./external-libs/SDL2-w64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image 
+
 echo starting...
 .\build\debug.exe
 @echo on
