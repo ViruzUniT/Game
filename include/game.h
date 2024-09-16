7e78fea2f4359a4a7d97aacba665238bd0b1414c
Bertasius <dmitrij.bertasius@cgi.com>
2024-09-16 12:16:16 +0200

    Cleaned up Main func(not Working)

--- include/game.h
+++ include/game.h
@@ -5,13 +5,48 @@
 #include "player.h"
 #include "renderer.h"
 #include "time.h"
+#include "timer.h"
 
 struct Game {
  public:
-  Game(const char* WindowName, int Width, int Height) : WindowName(WindowName), Width(Width), Height(Height) {}
-  ~Game() { delete WindowName; }
-  bool isRunning = true;
+  Game(const char* WindowName, int Width, int Height) : WindowName(WindowName), Width(Width), Height(Height) {
+    isRunning = true;
+    this->time = new Time(GetWindow());
+    std::vector<Entity*> EntityList;
+    Timer* startupTimer = new Timer();
 
+    RenderWindow window = GetWindow();
+
+    delete startupTimer;
+  }
+  ~Game() { delete WindowName; }
+
+  void RunGame() {
+    GameTexture playerIdleTexture = LoadTexture("Idle", "./sprites/Fighter/Idle.png", Vector4(46, 47, 30, 81), 98, 5, window);
+    Player Player(Vector2(100, 100), playerIdleTexture);
+    Player.AddTexture(LoadTexture("Walk", "./sprites/Fighter/Walk.png", Vector4(46, 45, 24, 83), 104, 7, window));
+
+    while (isRunning == true) {
+      time->StartMeasure();
+      while (time->accumulator >= time->timeStep) {
+        HandleSDLEvents(*time, Player);
+        time->accumulator -= time->timeStep;
+      }
+
+      window.Clear();
+      window.Render(Player);
+      window.Display();
+
+      time->EndMeasure();
+      time->FrameLimitPause();
+      time->ShowFPS();
+    }
+    window.CleanUp();
+    window.DestroyWindowAndRenderer();
+    SDL_Quit();
+  }
+
+  inline bool IsGameRunning() { return isRunning; }
   RenderWindow& GetWindow() { return window; }
 
   void HandleSDLEvents(Time& Time, Player& Player);
@@ -19,7 +54,12 @@ struct Game {
   GameTexture LoadTexture(const char* TextureName, const char* SpriteLocation, Vector4 CurrentFrame, int FrameOffset, int Frames,
       RenderWindow Window);
 
+  Time* time;
+  std::vector<Entity*> EntityList;
+
  private:
+  bool isRunning;
+
   const char* WindowName;
   int Width;
   int Height;
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,8 @@
 #include <Windows.h>
 
-#include <vector>
-
 #include "../include/game.h"
-#include "../include/player.h"
-#include "../include/timer.h"
-
-Game Game("Game v0.1", 825, 480);
-Time Time(Game.GetWindow());
-std::vector<Entity*> EntityList;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
-  Timer* startupTimer = new Timer();
-
-  RenderWindow window = Game.GetWindow();
-
-  GameTexture playerIdleTexture = Game.LoadTexture("Idle", "./sprites/Fighter/Idle.png", Vector4(46, 47, 30, 81), 98, 5, window);
-  Player Player(Vector2(100, 100), playerIdleTexture);
-  Player.AddTexture(Game.LoadTexture("Walk", "./sprites/Fighter/Walk.png", Vector4(46, 45, 24, 83), 104, 7, window));
-
-  delete startupTimer;
-
-  while (Game.isRunning == true) {
-    Time.StartMeasure();
-    while (Time.accumulator >= Time.timeStep) {
-      Game.HandleSDLEvents(Time, Player);
-      Time.accumulator -= Time.timeStep;
-    }
-
-    window.Clear();
-    window.Render(Player);
-    window.Display();
-
-    Time.EndMeasure();
-    Time.FrameLimitPause();
-    Time.ShowFPS();
-  }
-  window.CleanUp();
-  window.DestroyWindowAndRenderer();
-  SDL_Quit();
-  return 0;
+  Game Game("Game v0.1.5", 825, 480);
+  Game.RunGame();
 }
