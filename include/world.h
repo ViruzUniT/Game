b028d45bb5c3d7e961736b6156f566e65af253ad
Bertasius <dmitrij.bertasius@cgi.com>
2024-09-24 11:05:41 +0200

    formatation

--- include/world.h
+++ include/world.h
@@ -1,8 +1,12 @@
 #include "player.h"
 namespace World {
+Entity* FindEntityByName(const char* NameToFind);
+
 Entity* CreateEntity(const Vector2& position, GameTexture& Texture, Game& Game);
 Player* CreatePlayer(const Vector2& position, GameTexture& Texture, Game& Game);
+
 bool DestroyEntity(Entity*& Entity);
 bool DestroyPlayer(Player*& Player);
+
 void DestroyAllEntities();
 }  // namespace World
