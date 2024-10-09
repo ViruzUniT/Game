1cce2be9a894f05cba64456f3f24ae974be8521a
Bertasius <dmitrij.bertasius@cgi.com>
2024-10-09 14:48:14 +0200

    a

--- pipeline.bat
+++ pipeline.bat
@@ -1,12 +1,12 @@
-@echo compiling
-g++ -c -std=c++17 -g -Ofast src/*.cpp -Wall -m64 -lstdc++ -I ./build
-@echo compiling success
+REM @echo compiling
+REM g++ -c -std=c++17 -g -Ofast src/*.cpp -Wall -m64 -lstdc++ -I ./build
+REM @echo compiling success
 
 mkdir "./build/sprites"
 xcopy "./sprites" "./build/sprites" /E /Y
 
 move /Y "./*.o" "./build-int"
 
-@echo Linking
-g++ ./*.o -o build\debug.exe -L ./build -lmingw32 -lSDL2 -lSDL2_image 
-@echo Linking success
+REM @echo Linking
+REM g++ ./*.o -o build\debug.exe -L ./build -lmingw32 -lSDL2 -lSDL2_image 
+REM @echo Linking success
