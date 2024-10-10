2637cb51243836904b3d43e7e02176d93d75813d
Bertasius <dmitrij.bertasius@cgi.com>
2024-10-10 10:54:42 +0200

    Fixed all problems

--- debug.bat
+++ debug.bat
@@ -13,8 +13,8 @@ GOTO Done
 vendor\premake5.exe gmake2
 make
 
-copy "./MainGame/external-libs/SDL2-w64/bin/SDL2.dll" "./bin/Debug-windows/MainGame"
-copy "./MainGame/external-libs/SDL2-w64/bin/SDL2_image.dll" "./bin/Debug-windows/MainGame"
+copy "./SDL2.dll" "./bin/Debug-windows/MainGame"
+copy "./SDL2_image.dll" "./bin/Debug-windows/MainGame"
 mkdir "./bin/Debug-windows/MainGame/sprites"
 xcopy "./MainGame/sprites" "./bin/Debug-windows/MainGame/sprites" /E /Y
 
