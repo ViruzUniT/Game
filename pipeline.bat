46afb7a958197f2f5109fba651b921286b287c67
Bertasius <dmitrij.bertasius@cgi.com>
2024-10-10 10:55:33 +0200

    Fixed problems

--- pipeline.bat
+++ pipeline.bat
@@ -3,7 +3,7 @@ mkdir "./bin-int/Debug-windows/MainGame"
 mkdir "./bin/Debug-windows/MainGame"
 make
 
-copy "./MainGame/external-libs/SDL2-w64/bin/SDL2.dll" "./bin/Debug-windows/MainGame"
-copy "./MainGame/external-libs/SDL2-w64/bin/SDL2_image.dll" "./bin/Debug-windows/MainGame"
+copy "./SDL2.dll" "./bin/Debug-windows/MainGame"
+copy "./SDL2_image.dll" "./bin/Debug-windows/MainGame"
 mkdir "./bin/Debug-windows/MainGame/sprites"
 xcopy "./MainGame/sprites" "./bin/Debug-windows/MainGame/sprites" /E /Y
