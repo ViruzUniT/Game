6263074a37b9748c3a9f9ae0a1c93092f5091bae
Bertasius <dmitrij.bertasius@cgi.com>
2024-10-11 08:52:34 +0200

    added static linking

--- MainGame/premake5.lua
+++ MainGame/premake5.lua
@@ -28,14 +28,17 @@ extDir["SDL2"] = "external-libs/SDL2-w64/include/SDL2"
 
 filter { "configurations:Debug" }
 libdirs { "./external-libs/SDL2-w64/bin" }
-libdirs { "./external-libs/SDL2-w64/bin" }
 links { "SDL2", "SDL2_image" }
+buildoptions { "-static" }
 --buildoptions "/MTd"
 runtime "Debug"
 symbols "on"
 optimize "on"
 
 filter { "configurations:Release" }
+libdirs { "./external-libs/SDL2-w64/bin" }
+links { "SDL2", "SDL2_image" }
+buildoptions { "-static" }
 --buildoptions "/MT"
 runtime "Release"
 optimize "on"
