8cb0be1ce66d468701b4909741adb85220830159
Dima <dima@dima.com>
2025-03-03 14:53:28 +0100

    Grok has failed me but hopefully not twice

--- MainGame/premake5.lua
+++ MainGame/premake5.lua
@@ -1,41 +1,38 @@
-project "MainGame"
-kind "WindowedApp"
-language "C++"
-cppdialect "C++20"
-staticruntime "on"
-toolset "clang"
+project("MainGame")
+kind("WindowedApp")
+language("C++")
+cppdialect("C++20")
+staticruntime("on")
+toolset("clang")
 
 targetdir("../bin/" .. outputdir .. "/%{prj.name}")
 objdir("../bin-int/" .. outputdir .. "/%{prj.name}")
 
-files {
-  "src/**.cpp",
-  "include/**.h",
-  "src/**.h",
-  "include/**.cpp",
-  "include/**.c",
-  "external-libs/**.h",
-  "external-libs/SDL2-w64/include/SDL2/*.h",
-  "external-libs/**.c",
-  "external-libs/**.cpp"
-}
+files({
+	"src/**.cpp",
+	"include/**.h",
+	"src/**.h",
+	"include/**.cpp",
+	"include/**.c",
+	"external-libs/**.h",
+	"external-libs/SDL2-w64/include/SDL2/*.h",
+	"external-libs/**.c",
+	"external-libs/**.cpp",
+})
 
-defines {
-  "WINDOWS"
-}
+defines({
+	"WINDOWS",
+})
 
--- extDir = {}
--- extDir["SDL2"] = "external-libs/SDL2-w64/include/SDL2"
+filter({ "configurations:Debug" })
+libdirs({ "./external-libs/SDL2-w64/bin" })
+links({ "SDL2", "SDL2_image" })
+runtime("Debug")
+symbols("on")
+optimize("Debug")
 
-filter { "configurations:Debug" }
-libdirs { "./external-libs/SDL2-w64/bin" }
-links { "SDL2", "SDL2_image" }
-runtime "Debug"
-symbols "on"
-optimize "Debug"
-
-filter { "configurations:Release" }
-libdirs { "./external-libs/SDL2-w64/bin" }
-links { "SDL2", "SDL2_image" }
-runtime "Release"
-optimize "Speed"
+filter({ "configurations:Release" })
+libdirs({ "./external-libs/SDL2-w64/bin" })
+links({ "SDL2", "SDL2_image" })
+runtime("Release")
+optimize("Speed")
--- premake5.lua
+++ premake5.lua
@@ -1,33 +1,42 @@
-workspace "Game"
-architecture "x64"
-configurations {
-  "Debug",
-  "Release"
-}
-toolset "msc"
+workspace("Game")
+architecture("x64")
+configurations({
+	"Debug",
+	"Release",
+})
+toolset("msc")
 
-startproject "MainGame"
+startproject("MainGame")
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}"
 
-include "MainGame"
+include("MainGame")
 
-newaction {
-  trigger = "clean",
-  description = "Remove all binaries and intermediate binaries, and vs files.",
-  execute = function()
-    print("Removing binaries")
-    os.rmdir("./bin")
-    print("Removing intermediate binaries")
-    os.rmdir("./bin-int")
-    print("Removing project files")
-    os.rmdir("./.vs")
-    os.remove("**.sln")
-    os.remove("**.vcxproj")
-    os.remove("**.vcxproj.filters")
-    os.remove("**.vcxproj.user")
+postbuildcommands({
+	-- Copy SDL DLLs to the target directory
+	'{COPY} "SDL2.dll" "%{cfg.targetdir}"',
+	'{COPY} "SDL2_image.dll" "%{cfg.targetdir}"',
+	-- Create sprites directory and copy its contents
+	'{MKDIR} "%{cfg.targetdir}/sprites"',
+	'{COPY} "MainGame/sprites/*" "%{cfg.targetdir}/sprites"',
+})
 
-    os.remove("**Makefile")
-    print("Done")
-  end
-}
+newaction({
+	trigger = "clean",
+	description = "Remove all binaries and intermediate binaries, and vs files.",
+	execute = function()
+		print("Removing binaries")
+		os.rmdir("./bin")
+		print("Removing intermediate binaries")
+		os.rmdir("./bin-int")
+		print("Removing project files")
+		os.rmdir("./.vs")
+		os.remove("**.sln")
+		os.remove("**.vcxproj")
+		os.remove("**.vcxproj.filters")
+		os.remove("**.vcxproj.user")
+
+		os.remove("**Makefile")
+		print("Done")
+	end,
+})
