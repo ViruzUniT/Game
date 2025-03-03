181b77a241e910c7f810373ba2f7e9f2073cd4aa
Dima <dima@dima.com>
2025-03-03 11:45:10 +0100

    fix: improve pipeline

--- pipeline.bat
+++
@@ -1,9 +0,0 @@
-.\vendor\premake5.exe gmake2
-mkdir "./bin-int/Debug-windows/MainGame"
-mkdir "./bin/Debug-windows/MainGame"
-make
-
-copy "./SDL2.dll" "./bin/Debug-windows/MainGame"
-copy "./SDL2_image.dll" "./bin/Debug-windows/MainGame"
-mkdir "./bin/Debug-windows/MainGame/sprites"
-xcopy "./MainGame/sprites" "./bin/Debug-windows/MainGame/sprites" /E /Y
